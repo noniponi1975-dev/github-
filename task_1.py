e32ac7f09e500e9810a9dbfb1672b42b8d404d60
noniponi1975-dev <noniponi1975@gmail.com>
2026-02-10 22:15:27 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,15 @@
+a=float(input("enter your first number"))
+b=float(input("enter your second number"))
+#operations
+#addition
+sum=a+b
+print(sum)
+#subtraction
+sub=a-b
+print(sub)
+#multiplication
+mul=a*b
+print(mul)
+#division
+div=a/b
+print(div)
